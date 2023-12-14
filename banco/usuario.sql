e0751a546ea3b47808fb6b363e7910d15703b093
unknown <leilianeferreira53@gmail.com>
2023-12-14 01:02:41 -0300

    primeiro commit

---
+++ banco/usuario.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 11/11/2023 às 03:48
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `usuario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `user_form`
+--
+
+CREATE TABLE `user_form` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `user_form`
+--
+ALTER TABLE `user_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `user_form`
+--
+ALTER TABLE `user_form`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ evento03.php
@@ -0,0 +1,149 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['user_name'])){
+   header('location:login_form.php');
+}
+
+?>
+
+
+<!DOCTYPE HTML> 
+<html lang="pt-br"> 
+   <head>
+      <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NEUROVENDAS</title>
+    <link rel="icon" href="img/logo_faculdade.png"> 
+      <link rel="stylesheet" href=css/style_evento01.css> 
+   </head>
+
+   <body>
+
+
+ <nav>
+         <img class="logo" src="img/logo_nav.png" width="100px">
+         <h3> FACULDADE CDL </h3>
+         <ul class="nav_list"> 
+             <ul class="nav_list"> 
+               <div class="img_perfil">
+                  <img class="icone_perfil" src="img/icone_perfil.png">
+               </div>
+               <div class="nome_perfil"> 
+                  <p class="nome_perfil"><span><?php echo $_SESSION['user_name'] ?></span></p> 
+               </div>
+               <div class="img_sair"> 
+                  <input type="image" img class="icone_sair" src="img/icone_sair.png" onclick="openModal()"> 
+               </div>
+         </ul>
+      </nav>
+
+<center><h1>NEUROVENDAS</h1></center>
+
+  <div id="card_grande" style="height: 1420px;">
+            <p align="justify"> TÉCNICAS PARA AUMENTAR A EFICÁCIA DAS ABORDAGENS DE VENDAS E ESTIMULAR A TOMADA DE DECISÃO DOS COMPRADORES.<br><br>
+
+            Em um contexto de mudança de comportamento de compra e de interesse de venda se faz necessário fazer esse encontro assertivo entre o que e quem quer comprar e quem deseja vender. Porém, nem sempre é fácil, já que muitas vezes a pessoa que quer comprar não sabe exatamente o que deseja. Surge então a importância de se conhecer a mente do consumidor e ferramentas capazes de potencializar esse vendedor a fim de fechar a venda.</p>
+
+            <h3> OBJETIVO </h3>
+
+            <p align="justify"> 
+                Apresentar ferramentas e estratégias de vendas com base nas neurociências para potencializar o vendedor para o fechamento da venda, incluindo:<br><br>
+
+                • Conhecer mente dos clientes: o que buscam, querem e desejam;<br>
+                • Conhecer limites e formas de se potencializar como vendedor;<br>
+                • Conhecer as estratégias de comunicação assertiva para fechamento de vendas.<br>
+
+            <h3> CONTEÚDO PROGRAMÁTICO </h3>
+
+            <p align="justify">
+                • CONSUMIDOR – Neurociências e Comportamento do consumidor: Cérebro e decisão de compra, Sensação, emoção e percepção;<br>
+                • VENDEDOR – papel do vendedor, bloqueios e estratégias para potencializar-se;<br>
+                • CONEXÃO VENDEDOR/CONSUMIDOR E VENDA – Comunicação assertiva com o cliente: assertividade, Rapport e Escuta ativa, processos de venda com uso de gatilhos.<br>
+            </p>
+
+            <h3> METODOLOGIA </h3>
+
+            <p align="justify">
+
+                Para o curso serão utilizadas metodologias ativas, com reflexões e discussão de casos e de interação entre os participantes.
+            </p>
+
+
+            <h3> AO FINAL DO CURSO O PARTICIPANTE SERÁ CAPAZ DE: </h3>
+
+            <p align="justify">
+
+                • Entender o funcionamento da mente do consumidor frente ao seu processo de decisão de compra;<br>
+                • Utilizar estratégias para se potencializar enquanto vendedor;<br>
+                • Aplicar ferramentas de comunicação assertiva para fechar a venda.<br>
+
+            <h3> FACILITADORA </h3>
+
+            <p align="justify">
+                Alessandra Schiarantolla – Mestre em Administração de Empresas, especialista em Marketing com graduação em Administração e psicologia (em andamento), psicanalista, practitioner em programação neurolinguística, coach e mentora de negócios inovadores. Gestora no segmento educacional, professora de graduação e pós-graduação, palestrante e facilitadora de treinamentos nas áreas de liderança, motivação, relações interpessoais, marketing, vendas e comportamento.</p>
+
+            <h3> CARGA HORÁRIA </h3>
+
+            <p>12 h/a</p>
+
+            <h3> PERÍODO DE REALIZAÇÃO </h3>
+            <p align="justify">
+
+                11, 13, 18 e 20/JULHO/2023 – Aulas na 3ª e 5ª feira
+
+                Horário: 18h30 as 21h30</p>
+
+            <h3> CERTIFICAÇÃO </h3>
+
+            <p align="justify">
+                A Faculdade CDL concederá certificação de conclusão para os participantes com frequência mínima de 75% às aulas, que realizarem as atividades solicitadas pela facilitadora, dentro dos critérios estabelecidos por ela, durante o desenvolvimento do curso.
+            </p>
+        </div>
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.3653587636204!2d-38.52341505673259!3d-3.7302867990628346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c748546a9aa3fd%3A0xf89b53c24d2a3d3a!2sFaculdade%20CDL%20-%20A%20melhor%20escola%20de%20neg%C3%B3cios%20do%20Cear%C3%A1!5e0!3m2!1spt-BR!2sbr!4v1685534333824!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0; margin-top: 40px; margin-left: 545px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+         <br><br>
+
+
+   
+    <div class="card" id="card" style="margin-top: -2035px;">
+        <div class="card-front">
+            <center><img class="img_card_01" src="img/neuro.gif" width="270px" height="200px"></center>
+         
+        <p class="data_evento01"><b>11 JUL > 23 JUL</b> </p>
+        <p class="nome_evento01"> <b>NEUROVENDAS</b></p>
+        <p class="local_evento01"> Faculdade CDL </p>
+
+            <center><input class="botao_evento01" type="submit" value="GARANTA JÁ O SEU INGRESSO!" onclick="rotateCard()"></center> 
+        </div>
+        <div class="card-back">
+            <img class="img_card_02" src="img/qrCode_neurovendas.png">
+            <br>
+            <p class="text_escanear" align="center" style="color: #fff"> Escaneie o QR Code acima para acessar o ingresso </p>
+            <input type="image" img class="icone_voltar_card" src="img/icone_voltar_card.png" onclick="rotateCard2()">
+        </div>
+    </div>
+
+
+<div class="modal-container">
+    <div class="modal">
+      <center><h4>ATENÇÃO!</h4></center>
+      <hr>
+      <span>
+        Tem certeza que deseja sair?
+      </span>
+      <div class="btns">
+        <button class="btnOK" onclick= "location.href='logout.php'">Sim</button>
+        <button class="btnClose" onclick="closeModal()">Cancelar</button>
+      </div>
+    </div>
+  </div>
+
+
+<script src="js/script.js"></script>
+   </body>
+</html>
